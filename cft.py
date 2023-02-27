c377c3db4d91ad3d03bb13f2bae4b4116bfb0c73
Divyanshu Gupta <guptadv17@gmail.com>
2023-02-27 22:18:57 +0100

    added laguerre polynomials

--- cft.py
+++ cft.py
@@ -1,9 +1,12 @@
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jax import random, jit, vmap
+from jax import random, jit, vmap, lax
 from functools import partial
 from flax import struct
+
+import numpy as np
+from numpy.polynomial import Laguerre
 
 
 @jit
@@ -29,5 +32,52 @@ def reduced_partition_function_spinless(params, beta, c):
     characters = vmap(reduced_chi_delta, in_axes=(0,None,None), out_axes=0) # defines a vectorisable map for characters
     return jnp.sum(characters(params[0], beta, c) * params[1]) + reduced_chi_0(beta,c) 
     
-    
+@jit
+def laguerre(Ls,n, x):
+    """Laguerre odd recusision 
+        Params:
+        Ls: L_{n - 1} , L_{n + 1}
+        n: n
+        x: x
 
+        Returns:
+        (L_{n + 1} , L_{n + 2}) , L_{n + 2}
+    """
+    Lnm1, Ln = Ls
+    Lnp1 = ((2 * n + 1 - x) * Ln - n * Lnm1) / (n + 1)
+    Lnp2 = ((2 * (n + 1) + 1 - x) * Lnp1 - (n + 1) * Ln) / ((n + 1) + 1)
+    return  (Lnp1,Lnp2), Lnp2
+
+@jit
+def laguerre_with_derivatives(Ls,n, x):
+    
+    Lnm1, Ln = Ls
+    Lnp1 = ((2 * n + 1 - x) * Ln - n * Lnm1) / (n + 1)
+    Lnp2 = ((2 * (n + 1) + 1 - x) * Lnp1 - (n + 1) * Ln) / ((n + 1) + 1)
+    derLnp2 =  ((n + 1) + 1) * (Lnp2 - Lnp1) / (x)
+    return  (Lnp1,Lnp2), jnp.array([Lnp2, derLnp2])
+
+@jit
+def laguerre_derivates(Ls,n, x):
+    Lnm1, Ln = Ls
+    Lnp1 = ((2 * n + 1 - x) * Ln - n * Lnm1) / (n + 1)
+    Lnp2 = ((2 * (n + 1) + 1 - x) * Lnp1 - (n + 1) * Ln) / ((n + 1) + 1)
+    derLnp2 =  ((n + 1) + 1) * (Lnp2 - Lnp1) / (x)
+    return  (Lnp1,Lnp2), derLnp2   
+
+@partial(jit, static_argnames=['order'])
+def laguerre_at_x(order, x):
+    """Vector with odd Laguerre polynomials
+    """
+    x = 4 * jnp.pi * (x)
+    laguerre_x = partial(laguerre, x = x)
+    ns = jnp.arange(-1, order*2, step = 2, dtype=float)
+    ns = ns.at[0].add(1e-8)
+    _ , lps = lax.scan(laguerre_x, init=(1e-8, 0.0), xs=ns)
+    return lps
+
+@partial(jit, static_argnames=['order'])
+def laguerre_at_0(order, x):
+    """Vector with odd Laguerre polynomials at delta = 0
+    """
+    return laguerre_at_x(order, x) - 2 * jnp.exp(-2 * jnp.pi) * laguerre_at_x(order, x + 1) + jnp.exp(-4 * jnp.pi) * laguerre_at_x(order, x + 2) 
