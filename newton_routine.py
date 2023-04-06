89fdba570058542e337255f86e4a2817442913c5
Davide Passaro <d.passaro@uva.nl>
2023-04-06 10:50:14 +0200

    implemented hydra hypterparameters in newton_routine.py

--- newton_routine.py
+++ newton_routine.py
@@ -11,102 +11,116 @@ PATH = os.getcwd()
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 
+import hydra
 import optax
 from tqdm import tqdm
+from omegaconf import DictConfig
 
-from scipy import optimize
-
-
-
-jax.config.update("jax_enable_x64", True)
-jnp.set_printoptions(precision=20)
-
-#random number generator seed = 1
-rng = random.PRNGKey(1)
-
-# Define a model
-c = 12 # <- change this to generate values for a different 
-
-print(f"c = {c}")
-
-list_losses = []
-list_params = []
-list_guesses = []
-
-# Initialisations 1
-deltas = jnp.array([2.13, 3.4, 5.1]) # starting guess for deltas
-a_mus = jnp.ones(deltas.shape, dtype = float) # starting guess for a_mus is an array of ones
-params = jnp.array([deltas,a_mus])
-params = params.flatten()
-
-list_guesses.append(params)
-
-keys = random.split(rng, 2)
-nr_primal = partial(newton_rap,primal) # select which newton raphson to use: primal in this case
-
-print("Subroutine 1")
-# sub-routine 1 to get the first few parameters
-for run in range(10):
-    print(f" run {run} with number of deltas = {run + 3}")
-    # check whether params are optimal by using newton raphson
-    params, *others = nr_primal(x0 = params, c=c)
-    if others[-1]: # abort condition
-        print("Encountered NaN: aborting")
-        break
-    list_params.append(params)
-    # generate guesses for m+1 deltas using the previous m deltas
-    new_deltas, keys = initial_guess_generation(params, keys)
-    a_mus = jnp.ones(new_deltas.shape, dtype = float)
-    params = jnp.array([new_deltas,a_mus]).flatten()
-    list_guesses.append(params)
-
-
-print("Subroutine 2")
-# sub-routine 2 to get the higher order parameters with a more sophisticated guess generation scheme
-
-#Initialisations 2
-deltas_nm1 = list_params[-2][:len(list_params[-2])//2] # delta n - 1
-deltas_n = deltas_nm1 # delta n
-deltas_g = list_params[-1][:len(list_params[-1])//2] # delta guess
-adam = optax.adam(learning_rate=0.2)
-
-a_mus_g = list_params[-1][len(list_params[-2])//2+1:]
-
-h = len(deltas_g)
-nr_primal_normal = partial(newton_rap,primal_normal)
-for p in range(20):
-    print(f"run {p} with number of deltas = {h}")
+@hydra.main(version_base='1.2', config_path="conf", config_name="cft")
+def main(cfg: DictConfig):
+    jax.config.update("jax_enable_x64", True)
+    jnp.set_printoptions(precision=cfg.jax_settings.precision)
     
-    # guess generation for a_mus -> Optimise over a_mus with fixed deltas 
-    a_mus_g, losses, _ = rhos_optim(a_mus_g, deltas_g, c, opt_fn=adam.update, opt_state=adam.init(a_mus_g), steps=2000)
-
-    params = jnp.array([deltas_g,a_mus_g])
+    #random number generator seed = 1
+    rng = random.PRNGKey(cfg.random_seed)
+    
+    # Define a model
+    c = cfg.model.central_charge # <- change this to generate values for a different 
+    
+    print(f"central charge: c = {c}")
+    
+    list_losses = []
+    list_params = []
+    
+    # Initialisations at step 1
+    deltas = hydra.utils.instantiate(cfg.model.initializations.deltas) # starting guess for deltas
+    a_mus = jnp.ones(deltas.shape, dtype = float) # starting guess for a_mus is an array of ones
+    params = jnp.array([deltas,a_mus])
     params = params.flatten()
-
-    # newton method
-    params, *others = nr_primal_normal(params,c=c)
-
-    if others[-1]: # abort condition
-        print("Encountered NaN: aborting")
-        break 
-
-    list_params.append(params)
-    list_losses.append(primal_normal(params,c=c))
-
-    deltas_nm1 = deltas_n
-    deltas_n = lax.dynamic_slice(params, (0,),(h,))
-    a_mus_n = lax.dynamic_slice(params, (h,),(h,))
     
-    # Generate guess for delta_{n + 1} using delta_{n} and deltas_{n - 1}
-    deltas_g = guess_generator(deltas_nm1, deltas_n, fixed_point=1) 
-    a_mus_g = jnp.ones(deltas_g.shape, dtype = float) 
-    a_mus_g = a_mus_g.at[0:h].set(a_mus_n)
+    keys = random.split(rng, 2)
+    nr_primal = partial(newton_rap,primal) # select which newton raphson to use: primal in this case
+    
+    print("Subroutine 1")
+    # sub-routine 1 to get the first few parameters
+    run = 0
+    while True:
+        print(f" run {run} with number of deltas = {run + 3}")
+        # check whether params are optimal by using newton raphson
+        params, *others = nr_primal(x0 = params, c=c)
+        if others[-1]: # abort condition
+            print("Encountered NaN: aborting")
+            break
+        list_params.append(params)
+        # generate guesses for m+1 deltas using the previous m deltas
+        new_deltas, keys = initial_guess_generation(params, keys)
+        a_mus = jnp.ones(new_deltas.shape, dtype = float)
+        params = jnp.array([new_deltas,a_mus]).flatten()
+        run += 1
+    
+    assert len(list_params)>1, "Subroutine 1 failed to generate any parameters. Subroutine 2 cannot proceed."
+    
+    print("Subroutine 2")
+    # sub-routine 2 to get the higher order parameters with a more sophisticated guess generation scheme, but with at least two known parameters.
+    
+    #Initialisations 2
+    deltas_nm1 = list_params[-2][:len(list_params[-2])//2] # delta n - 1
+    deltas_n = deltas_nm1 # delta n
+    deltas_g = list_params[-1][:len(list_params[-1])//2] # delta guess
+    opt = optax.adam(cfg.training.learning_rate)
+    
+    a_mus_g = list_params[-1][len(list_params[-2])//2+1:]
+    
+    h = len(deltas_g)
+    nr_primal_normal = partial(newton_rap,primal_normal)
+    
+    h_offset = jnp.copy(h)
 
-    list_guesses.append(deltas_g)
-    h += 1
+    print(opt, type(opt))
+    while True:
+        print(f"run {h-h_offset} with number of deltas = {h}")
+        
+        # guess generation for a_mus -> Optimise over a_mus with fixed deltas 
+        # mean square loss for the primal function over a_mus
+        opt_state =  opt.init(a_mus_g)
+        a_mus_g, losses, _ = rhos_optim(a_mus_g, deltas_g, c, opt_fn=opt.update, opt_state= opt_state, steps=cfg.training.optimizer_steps)
+    
+        params = jnp.array([deltas_g,a_mus_g])
+        params = params.flatten()
+    
+        # newton method
+        params, *others = nr_primal_normal(params,c=c)
+    
+        if others[-1]: # abort condition
+            print("Encountered NaN: aborting")
+            break 
+    
+        list_params.append(params)
+        list_losses.append(primal_normal(params,c=c))
+    
+        deltas_nm1 = deltas_n
+        deltas_n = lax.dynamic_slice(params, (0,),(h,))
+        a_mus_n = lax.dynamic_slice(params, (h,),(h,))
+        
+        # Generate guess for delta_{n + 1} using delta_{n} and deltas_{n - 1}
+        deltas_g = guess_generator(deltas_nm1, deltas_n, fixed_point=1) 
+        a_mus_g = jnp.ones(deltas_g.shape, dtype = float) 
+        a_mus_g = a_mus_g.at[0:h].set(a_mus_n)
+    
+        h += 1
+    
+    if not (len(list_params) == 0):
+        savedir = f"{PATH}/spinless-parameters/c-{c}"
+        save = True
+        if os.path.isdir(savedir):
+            saved_params = jnp.load(savedir+".npy", allow_pickle=True)
+            if len(saved_params) >= len(list_params):
+                save = False
+        if save:
+            print("Saving parameters in directory spinless-parameters")
+            jnp.save(f"{PATH}/spinless-parameters/c-{c}",np.array(list_params, dtype=object))
+        else:
+            print("Nothing new to save")
 
-if not (len(list_params) == 0):
-    print("Saving parameters in directory spinless-parameters")
-    jnp.save(f"{PATH}/spinless-parameters/c-{c}",np.array(list_params, dtype=object))
-
-# To load c4 = jnp.load(f"{PATH}/spinless-parameters/c-4.npy", allow_pickle=True)
+if __name__ == "__main__":
+    main()
